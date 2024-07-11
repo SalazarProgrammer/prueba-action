3e8939ee1387dca0265bfe09af49eee0fe85ce80
SalazarProgrammer <000jsalazar@gmail.com>
2024-07-11 16:25:56 -0400

    Primer commit de prueba Github Action.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Action</title>
+</head>
+<body>
+<h1>Prueba Action</h1>
+<h2>Primera prueba: Archivo enviado desde Git a Servidor.</h2>
+</body>
+</html>
